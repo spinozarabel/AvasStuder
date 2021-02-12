f619ecf4195c4ddc62c5001627474cdfd4db4a5b
spinozarabel <>
2021-02-12 12:34:27 +0530

    Update avas_studer.php

--- avas_studer.php
+++ avas_studer.php
@@ -51,6 +51,7 @@ function studer_readings_page_render()
   ?>
     <div class="container-fluid">
       <img src="https://sritoni.org/6076/wp-content/uploads/sites/14/2021/02/electrical-grid-clipart.jpg" alt="grid" style="width:10%;">
+      <img src="https://sritoni.org/6076/wp-content/uploads/sites/14/2021/02/right_arrow.jpg" alt="grid" style="width:10%;">
     </div>
   <?php
 
