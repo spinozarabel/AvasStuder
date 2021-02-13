dfbd92cec57b7d98d7f3c562ed20f1a21e58b670
spinozarabel <>
2021-02-13 10:07:36 +0530

    Update avas_studer.php

--- avas_studer.php
+++ avas_studer.php
@@ -170,9 +170,10 @@ function studer_readings_page_render()
   }
 
   // calculate the current into/out of battery
-  $battery_charge_adc = $solar_pv_adc + $inverter_current_adc; // + is charge, - is discharge
-  $pbattery_kw        = round($psolar_kw - $pout_inverter_ac_kw, 3);
-  $psolar_kw          = round($psolar_kw,3);
+  $battery_charge_adc  = $solar_pv_adc + $inverter_current_adc; // + is charge, - is discharge
+  $pbattery_kw         = round($psolar_kw - $pout_inverter_ac_kw, 3);
+  $psolar_kw           = round($psolar_kw,3);
+  $pout_inverter_ac_kw = round($pout_inverter_ac_kw,3);
 
 
   ?>
