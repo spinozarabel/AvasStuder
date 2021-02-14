b48500f0bd3b873885a47f62a4ae0d92cfb6d9f2
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2021-02-14 12:06:11 +0530

    Create update.js

---
+++ update.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function($) {
+
+  // set an intervel of 3s. The callback function that gets
+  // executed at the end is passed as timingload() that needs to be defined
+  setInterval(timingLoad, 3000);
+  //
+  function timingLoad() {
+
+    $.post(my_ajax_obj.ajax_url,
+    {                                 //POST request
+      _ajax_nonce: my_ajax_obj.nonce, //nonce extracted and sent
+      action: "getMyStuderReadings"  // hook added for action wp_ajax_spzrbl_city in php file
+    },
+      function(data) 	{				// data is JSON data sent back by server in response, wp_send_json($somevariable)
+
+      });
+  };
+
+});
