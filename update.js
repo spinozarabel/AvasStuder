48205199dc89f9c068b49f72d4b48aef86dfd888
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2021-02-14 16:22:56 +0530

    Update update.js

--- update.js
+++ update.js
@@ -3,6 +3,7 @@ jQuery(document).ready(function($) {
   // set an intervel of 3s. The callback function that gets
   // executed at the end is passed as timingload() that needs to be defined
   //setInterval(timingLoad, 60000);
+  timingLoad();
 
   //
   function timingLoad() {
