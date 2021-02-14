a41f4d992474e7f0ee99f93cedb7d84383ad5047
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2021-02-14 14:43:06 +0530

    Update update.js

--- update.js
+++ update.js
@@ -2,7 +2,8 @@ jQuery(document).ready(function($) {
 
   // set an intervel of 3s. The callback function that gets
   // executed at the end is passed as timingload() that needs to be defined
-  // setInterval(timingLoad, 3000);
+  //setInterval(timingLoad, 3000)
+  timingLoad();
   //
   function timingLoad() {
 
@@ -12,7 +13,9 @@ jQuery(document).ready(function($) {
       action: "ajax_get_studer_readings"  // hook added for action wp_ajax_spzrbl_city in php file
     },
       function(data) 	{				// data is JSON data sent back by server in response, wp_send_json($somevariable)
-        // update the page with new readings
+        // update the page with new readings. Lets just log the value sto see if we are getting good data
+        console.log('data: ', data);
+
       });
   };
 
