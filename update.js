33f4f5caf633733be597cfd6cdbac80c8fca113e
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2021-02-15 08:07:59 +0530

    Update update.js

--- update.js
+++ update.js
@@ -2,9 +2,8 @@ jQuery(document).ready(function($) {
 
   // set an intervel of 3s. The callback function that gets
   // executed at the end is passed as timingload() that needs to be defined
-  // setInterval(timingLoad, 60000);
+  setInterval(timingLoad, 10000);
   // console.log('my_ajax_obj: ', my_ajax_obj);
-  timingLoad();
 
   //
   function timingLoad() {
@@ -16,8 +15,11 @@ jQuery(document).ready(function($) {
     },
       function(data) 	{				// data is JSON data sent back by server in response, wp_send_json($somevariable)
         // update the page with new readings. Lets just log the value sto see if we are getting good data
-        console.log('data: ', data);
+        // console.log('data: ', data);
+        var powerLoad = $('#power-load');	// select form element with id="power-load"
+            powerLoad.val(data.pout_inverter_ac_kw + ' kW');
 
+        $('#power-arrow-load').removeClass().addClass(data.inverter_pout_arrow_class);
       });
   };
 
