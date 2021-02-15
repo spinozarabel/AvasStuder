8787c0bb77224371f6e48bb0dfe19ad8ff53550a
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2021-02-15 15:24:24 +0530

    Update update.js

--- update.js
+++ update.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
 
   // set an intervel of 3s. The callback function that gets
   // executed at the end is passed as timingload() that needs to be defined
-  // setInterval(timingLoad, 10000);
+  setInterval(timingLoad, 20000);
   // console.log('my_ajax_obj: ', my_ajax_obj);
 
   //
@@ -16,10 +16,29 @@ jQuery(document).ready(function($) {
       function(data) 	{				// data is JSON data sent back by server in response, wp_send_json($somevariable)
         // update the page with new readings. Lets just log the value sto see if we are getting good data
         console.log('data: ', data);
-        var powerLoad = $('#power-load');	// select form element with id="power-load"
-            powerLoad.val(data.pout_inverter_ac_kw + ' kW');
 
+        //Change Inverter output power value using Ajax delivered object data
+        $('#power-load').val(data.pout_inverter_ac_kw + ' kW');
+        // change the arrow class for Inverter Pout to Home using Ajax update
         $('#power-arrow-load').removeClass().addClass(data.inverter_pout_arrow_class);
+
+        // Solar Power related values Ajax update
+        //Change Solar output power value using Ajax delivered object data
+        $('#power-solar').val(data.psolar_kw + ' kW');
+        // todo need to add the SOlar-PB current at battery interface
+        // update the arrow based on ajax
+        $('#power-arrow-solar').removeClass().addClass(data.solar_arrow_class);
+
+        // Change the Battery values based on Ajax update
+        $('#power-arrow-battery').removeClass().addClass(data.battery_charge_arrow_class);
+        //Change Inverter output power value using Ajax delivered object data
+        $('#power-battery').val(data.pbattery_kw + ' kW');
+
+        //Change Grid AC in power and arrow calss based on Ajax updates
+        //Change Inverter output power value using Ajax delivered object data
+        $('#ppower-grid-genset').val(data.grid_pin_ac_kw + ' kW');
+        // change the arrow class for Inverter Pout to Home using Ajax update
+        $('#power-arrow-grid-genset').removeClass().addClass(data.grid_input_arrow_class);
       });
   };
 
