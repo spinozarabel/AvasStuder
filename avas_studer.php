e61efba9c0535a4dbf1c690f608ebda66b50191a
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2021-02-18 12:09:50 +0530

    Update avas_studer.php

--- avas_studer.php
+++ avas_studer.php
@@ -184,6 +184,7 @@ function studer_main_page_render()
   $studer_api->device               = 'XT1';
   $studer_api->paramPart            = 'Value';
   $battery_uv_compensation          = $studer_api->get_parameter_value();
+  ($battery_uv_compensation == 1 ? 'yes' : 'No');
 
   $studer_api->paramId              = 1532;
   $studer_api->device               = 'XT1';
