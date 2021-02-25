3967c5142709fe2db873bece6cee682612497b8a
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2021-02-25 11:38:42 +0530

    Update avas_studer.php

--- avas_studer.php
+++ avas_studer.php
@@ -1118,7 +1118,7 @@ function get_studer_readings()
   else
   {
     // Transfer relay is open and grid input is not possible
-    $grid_input_arrow_class = "fa fa-times-circle fa-1x";
+    $grid_input_arrow_class = "fa fa-times-circle fa-2x";
   }
 
   switch(true)
